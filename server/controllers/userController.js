2bce0f6f267b5be394b9c9c88a236dddc18cf0a4
newbeeguy <mskjavascript@gmail.com>
2021-03-01 20:16:10 +0300

    new

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,19 +1,43 @@
-const {User} = require('../models/models')
+const {User, Basket} = require('../models/models')
 
+const bcrypt = require('bcrypt')
 
 const ApiError = require('../error/ApiError')
+const jwt = require("jsonwebtoken");
+
+
+const generateJwt = (id, email, role) => {
+   return jwt.sign({id ,email,role},process.env.SECRET_KEY,
+        {expiresIn: "24h"})
+}
 
 
 class UserController {
-    async registration( req, res) {
+    async registration(req, res, next) {
+        let {email, password, role} = req.body
+
+        if (!email || !password) {
+            return next(ApiError.badRequest('Некорректный меил или пароль'))
+        }
+        const candidate = await User.findOne({where: {email}})
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь уже существует'))
+
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email , password: hashPassword, role })
+        const basket = await Basket.create({userId: user.id})
+        let token =  generateJwt(user.id, user.email,user.role)
+        return res.json({token})
+    }
+
+    async login(req, res) {
 
     }
-    async login( req, res) {
 
-    }
-    async check( req, res, next) {
+    async check(req, res, next) {
         const {id} = req.query
-        if(!id){
+        if (!id) {
             return next(ApiError.badRequest("Не указан id"))
         }
         res.json(id)
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,9 +5,8 @@ const userRouter = require('./userRouter')
 const typeRouter = require('./typeRouter')
 const brandRouter = require('./brandRouter')
 
-
-router.use('/device',deviceRouter)
 router.use('/user',userRouter)
+router.use('/device',deviceRouter)
 router.use('/type',typeRouter)
 router.use('/brand',brandRouter)
 
