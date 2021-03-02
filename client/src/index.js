021235f6e65e5b2874175fcd8bf3a905d415104d
newbeeguy <mskjavascript@gmail.com>
2021-03-02 15:56:03 +0300

    fixed with .env

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,11 @@ import App from './App';
 import UserStore from "./store/UserStore";
 import DeviceStore from "./store/DeviceStore";
 
+
+
+console.log(process.env.REACT_APP_API_URL)
+
+
 export const Context = createContext(null )
 
 ReactDOM.render(
