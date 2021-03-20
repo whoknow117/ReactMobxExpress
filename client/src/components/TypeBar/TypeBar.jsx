71e1253762fb4dd956c68cdacd4e1ec48e3c1813
newbeeguy <mskjavascript@gmail.com>
2021-03-20 15:00:39 +0300

    fixed

--- client/src/components/TypeBar/TypeBar.jsx
+++ client/src/components/TypeBar/TypeBar.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import  React, {useContext, useEffect, useState} from 'react';
 import classes from './TypeBar.module.scss';
 import {Context} from "../../index";
 import ListGroup from "react-bootstrap/ListGroup";
