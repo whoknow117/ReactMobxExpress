712985995f959ddf554f41c449370d7b8b6b030b
newbeeguy <mskjavascript@gmail.com>
2021-04-02 15:18:33 +0300

    fixed

--- client/src/pages/Basket/Basket.jsx
+++ client/src/pages/Basket/Basket.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import classes from './Basket.module.scss';
+import {Context} from "../../index";
+import {observer} from "mobx-react-lite";
 
-const Basket = () => {
+const Basket = observer(() => {
+
+    const {device} = useContext(Context);
+
+    const [storageCart, setStorageCart] = useState(device.storageCart)
+
+    useEffect(() => {
+        let updatedBasket = localStorage.getItem('cart')
+        if( updatedBasket) {
+            device.setStorageCart(JSON.parse(updatedBasket))
+        }
+
+
+    },[device.cartCounter])
+
+
+    debugger;
     return (
-        <div>
-            
+        <div className={classes.basketWrapper}>
+            <h1 className={classes.pageTitle}>Корзина заказов</h1>
+            {device.storageCart.map(el => {
+                return (
+                    <div className={classes.basketItem}
+                    key={el.id}
+
+                    >
+
+
+                        <div className={classes.image}>
+                            <img src={process.env.REACT_APP_API_URL + el.img} alt=""/>
+                        </div>
+                        <div className={classes.nameBlock}>
+                            <div className={classes.name}>{el.name}</div>
+                            <div className={classes.btnWrapper}>
+                                <button className={classes.btn}>
+                                    delete
+                                </button>
+                            </div>
+                        </div>
+                        <div className={classes.input}>
+                            <span className={classes.prev}>  </span>
+                            <span className={classes.next}> </span>
+
+                            <div >
+                                <span>0</span>
+                            </div>
+                        </div>
+                        <div></div>
+
+
+                    </div>
+                )
+            })}
         </div>
     );
-};
+});
 
 export default Basket;
